069cb932702b09ee18bf44072265e8b434595bd0
Giulia <81244332+GiuliaCarvalhoS@users.noreply.github.com>
2021-10-07 14:00:09 -0300

    Add files via upload

---
+++ add_itens.py
@@ -0,0 +1,68 @@
+
+	
+
+def atribuicaoValoresTabela():
+
+	matriz = [[' P',' Q',' ¬P' ,' ¬Q'],
+	          [True, True,False,False],
+				[True, False, False,True],
+				[False, True,True,False],
+			[False, False,True,True]]
+
+	def calcularImplicacao(matriz):
+		
+		
+
+		if(matriz[1:2] == matriz[1:1] ):
+		
+			for i in matriz:
+				for l in range(5):
+
+		if(matriz[1:2] == matriz[1:1] ):
+		if(matriz[1:2] == matriz[1:1] ):
+		if(matriz[1:2] == matriz[1:1] ):
+			
+
+		
+	    	
+
+
+
+
+	
+
+
+	
+	# calcularImplicacao(matriz)
+	
+	
+	calcularImplicacao(matriz)
+atribuicaoValoresTabela()
+
+	# for i in range(4):
+	# 	linha = [];
+	# 	for j in range(4):
+	# 		var ='';
+	# 		if(j == 0):
+	# 			var = 'p';
+	# 		elif(j == 1):
+	# 			var = 'q'
+	# 		elif(j==2):
+	# 			var = '¬p';
+	# 		else:
+	# 			var='¬q'
+
+	# 		elemento = str(input("informe o valor para linha {} e da colunha {}: ".format(i,var)))
+	# 		linha.append(elemento)
+	# 	matriz.append(linha);
+		
+		# def matriz_exibicao(matriz):
+	# 	for linha in matriz:
+	# 		print(linha)
+	# matriz_exibicao(matriz)
+	
+	
+
+
+
+
