deab1ac23e5ad9ff463179d158fc14923c42d697
Giulia <giuliacarvalhos15@gmail.com>
2021-10-14 22:52:32 -0300

    Co-authored-by: André Delfino <DehDelfino@users.noreply.github.com> Co-authored-by: Fernando Dias <FernandoDias18@users.noreply.github.com>

--- TabelaVerdde2.py
+++ TabelaVerdde2.py
@@ -1,19 +1,36 @@
 #class TabelaVerdade:
 
 #def atribuicaoValoresTabela():
-tabela = [["p" , "q", "~p", "~q", "p->q", "p^q","p v q", "p<->q"]]
+tabela = ["p" ,  "q",  "~p",  "~q",  "p->q",  "p^q",  "p v q",  "p<->q"]
 matriz = [[True, True, False, False],
          [True, False, False, True],
-         [False, True, True, False],
+         [False, True, True, False ],
          [False, False, True, True]]
 
 # nao a OU b
-#def inplitcacaoPemQ(primeiroItem,):
+def inplitcacaoPemQ():
+  primeiroItem4 = matriz[0][2] or matriz[0][1]
+  segundoItem4 = matriz[1][2] or matriz[1][1]
+  terceiroItem4 = matriz[2][2] or matriz[2][1]
+  quartoItem4 = matriz[3][2] or matriz[3][1]
 
-primeiroItem4 = matriz[0][2] or matriz[0][1]
-segundoItem4 = matriz[1][2] or matriz[1][1]
-terceiroItem4 = matriz[2][2] or matriz[2][1]
-quartoItem4 = matriz[3][2] or matriz[3][1]
+  for i in matriz:
+    for l in i:
+      print(l)
+  
+
+
+
+
+
+  return matriz
+
+print("aki roda")
+oi= inplitcacaoPemQ()
+print("matriz:")
+
+
+
 
 #aqui tem q ver como add uma fileira na matriz pra ficar igual o doc dela no ex 4
 
@@ -24,6 +41,7 @@ segundoItem5 = matriz[1][0] and matriz[1][1]
 terceiroItem5 = matriz[2][0] and matriz[2][1]
 quartoItem5 = matriz[3][0] and matriz[3][1]
 
+
 #aqui tem q ver como add uma fileira na matriz pra ficar igual o doc dela no ex 5
 
 #def inplitcacaoPouQ():
@@ -31,6 +49,7 @@ primeiroItem6 = matriz[0][0] or matriz[0][1]
 segundoItem6 = matriz[1][0] or matriz[1][1]
 terceiroItem6 = matriz[2][0] or matriz[2][1]
 quartoItem6 = matriz[3][0] or matriz[3][1]
+
 #aqui tem q ver como add uma fileira na matriz pra ficar igual o doc dela no ex 6
 
 #def inpitcacaoPouQ():
@@ -38,8 +57,6 @@ primeiroItem7 = ((matriz[0][2] or matriz[0][1]) and (matriz[0][3] or matriz[0][0
 segundoItem7 = ((matriz[1][2] or matriz[1][1]) and (matriz[1][3] or matriz[1][0]))
 terceiroItem7 = ((matriz[2][2] or matriz[2][1]) and (matriz[2][3] or matriz[2][0]))
 quartoItem7 = ((matriz[3][2] or matriz[3][1]) and (matriz[3][3] or matriz[3][0]))
-print(primeiroItem7)
-print(segundoItem7)
-print(terceiroItem7)
-print(quartoItem7)
+
 #aqui tem q ver como add uma fileira na matriz pra ficar igual o doc dela no ex 7
+
